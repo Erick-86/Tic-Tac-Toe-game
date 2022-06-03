e399b3493ffcd2bfb0ab49c7682810ba2ddffc44
freeman <zelonaire@gmail.com>
2022-06-03 20:38:03 +0000

    opening and closing of the game config modal

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
+//Modal Overlay
 const modalOverlay = document.getElementById("modal-overlay");
+//Game config modal
 const gameConfigModal = document.getElementById("game-config-modal");
-const cancelBtn = document.getElementById("cancel-btn")
-const editNameBtn = document.getElementById("edit-name-btn")
+//Edit player names btns
+const editElayerNameBtn1 = document.getElementById("edit-player1-name-btn");
+const editElayerNameBtn2 = document.getElementById("edit-player2-name-btn");
+//Modal cancel btn
+const closeGameConfigBtn = document.getElementById("cancel-btn")
+// const editNameBtn = document.getElementById("edit-name-btn")
+
 
 //Opening and closing fo the game config modal
-//Opening the modal
-function openModal() {
+// 
+// //Opening the modal
+function openGameConfig() {
     modalOverlay.style.display = "block"
     gameConfigModal.style.display = "block"
 }
-editNameBtn.addEventListener('click', openModal)
 
-//Closing the modal when clicking the overlay
-function closeModal() {
+editElayerNameBtn1.addEventListener("click", openGameConfig)
+editElayerNameBtn2.addEventListener("click", openGameConfig)
+
+// //Closing the modal
+function closeGameConfig() {
     modalOverlay.style.display = "none"
     gameConfigModal.style.display = "none"
 }
-modalOverlay.addEventListener('click', closeModal)
-
-//Closing the modal with cancel btn
-function closeModal() {
-    modalOverlay.style.display = "none"
-    gameConfigModal.style.display = "none"
-}
-cancelBtn.addEventListener('click', closeModal)
+modalOverlay.addEventListener('click', closeGameConfig)
+closeGameConfigBtn.addEventListener('click', closeGameConfig)
